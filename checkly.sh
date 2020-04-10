6fda98221e6b7767eede5df656039dfeba88a212
Tim Nolet <timnolet@hotmail.com>
2020-04-10 19:35:06 +0200

    ci: move script to sh file

---
+++ checkly.sh
@@ -0,0 +1,21 @@
+# Some initial variables
+STATUS=
+PASSING=
+CHECK_RESULT=
+
+# Call the Checkly trigger
+CHECKLY_URL="https://api-test.checklyhq.com/check-groups/4/trigger/$CHECKY_TOKEN"
+echo "Running your Checkly checks"
+CHECK_RESULT=$(curl -s $CHECKLY_URL)
+STATUS=$(echo $CHECK_RESULT | grep -c '"hasFailures":true')
+
+# Report the status
+if [ $STATUS -ne 0 ]; then
+  echo "Checkly checks failed"
+  PASSING=1
+else 
+  echo "Checkly checks passed"
+  PASSING=0
+fi
+
+exit $PASSING
